36d70c076f461b743dd4acafc6f06d2c1f2d3d31
Anders Gorm Pedersen <agpe@dtu.dk>
2025-12-14 08:52:41 +0100

    fix/docs: do not set .sem which is not available anymore; elaborate on trackroot error

--- phylotreelib.py
+++ phylotreelib.py
@@ -5578,17 +5578,19 @@ class TreeSummary():
         NOTE: requires rooting to be tracked in order to properly split branch length on 
         root bipartition"""
 
-        for p in sumtree.sorted_intnodes():
-            if p != sumtree.root:
-                for c in sumtree.children(p):
-                    bipart1 = sumtree.remotechildren_dict[c]
-                    bip = Bipartition(bipart1, sumtree.frozenset_leaves,
-                                      sumtree.sorted_leaf_list, sumtree.leaf2index)
-                    brstruct = self.bipartsummary[bip]
-                    sumtree.set_branch_attribute(p,c,"length", brstruct.length)
-                    sumtree.set_branch_attribute(p,c,"length_var", brstruct.length_var)
-                    sumtree.set_branch_attribute(p,c,"length_sd", brstruct.length_sd)
-                    sumtree.set_branch_attribute(p,c,"length_sem", brstruct.length_sem)
+        if not self.trackroot:
+            raise TreeError("Root was not tracked - impossible to use biplen on this summary tree\n"
+                            "(needed in order to properly split branch length on root bipartition)")
+
+        for p in sumtree.sorted_intnodes() - {sumtree.root}:
+            for c in sumtree.children(p):
+                bipart1 = sumtree.remotechildren_dict[c]
+                bip = Bipartition(bipart1, sumtree.frozenset_leaves,
+                                  sumtree.sorted_leaf_list, sumtree.leaf2index)
+                brstruct = self.bipartsummary[bip]
+                sumtree.set_branch_attribute(p,c,"length", brstruct.length)
+                sumtree.set_branch_attribute(p,c,"length_var", brstruct.length_var)
+                sumtree.set_branch_attribute(p,c,"length_sd", brstruct.length_sd)
 
         # Handle root bipartition separately
         kid1,kid2 = sumtree.children(sumtree.root)
